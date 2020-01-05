3a720a376521d7f8805578b8cb89078e94971cc7
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-01-05 20:46:35 +0530

    finally done

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,9 @@
 export const FirebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+	apiKey: 'AIzaSyB22-DZ_PvhTJsA-PPf8Vs--fmcCixL-YQ',
+	authDomain: 'cerebro-2018-f1052.firebaseapp.com',
+	databaseURL: 'https://cerebro-2018-f1052.firebaseio.com',
+	projectId: 'cerebro-2018-f1052',
+	storageBucket: 'cerebro-2018-f1052.appspot.com',
+	messagingSenderId: '1059299836137'
+	// appId: '1:1059299836137:web:1e0d0b9cb2b44c865bb078'
 };
